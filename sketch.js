0fa62517ed675b217b4731af66e32462421670d5
Bubs1048 <78613471+Bubs1048@users.noreply.github.com>
2021-02-27 19:26:00 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,62 +1,79 @@
+var starImg, fairyImg, bgImg;
+var fairy, fairyVoice;
+var star, starBody;
+var staticOption;
+var gravityOption;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var engine,world;
-var ground,sand,iron;
+function preload() {
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png", "images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
 
 function setup() {
-	createCanvas(800, 600);
+	createCanvas(800, 750);
+
+
+	fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying", fairyImg);
+	fairy.scale = 0.25;
+
+	star = createSprite(650, 30);
+	star.addImage(starImg);
+	star.scale = 0.2;
 
 	engine = Engine.create();
 	world = engine.world;
 
-	ground=new Ground(600,height,1200,15)
+	starBody = Bodies.circle(650, 30, 5, { restitution: 0.5, isStatic: true });
+	World.add(world, starBody);
 
-   hammer=new Hammer(200,200);
+	Engine.run(engine);
 
-   stone=new Stone(700,470,80,100 );
-
-   iron=new Iron(300,470,80,50);
-
-   rubber=new Rubber(700,370,20,10);
-
-   sand1=new Sand(250,470);
-   sand2=new Sand(270,470);
-   sand3=new Sand(390,470);
-   sand4=new Sand(400,470);
-   sand5=new Sand(410,470);
-   sand6=new Sand(280,470);
-
-   //Engine.run(engine);
-  
 }
 
 
 function draw() {
- 
-  rectMode(CENTER);
+	background(bgImg);
 
-  background("lightblue");
+	//   Engine.update(engine);
+	star.x = starBody.position.x;
+	star.y = starBody.position.y;
 
-  Engine.update(engine);
+	console.log(star.y);
+	if(star.y > 470 && starBody.position.y > 470 ){
+		Matter.Body.setStatic(starBody,true);
+	}
+	//   keyPressed();
 
-  hammer.display();
-  ground.display();
-  stone.display();
-  iron.display();
-  rubber.display();
-  sand1.display();
-  sand2.display();
-  sand3.display();
-  sand4.display();
-  sand5.display();
-  sand6.display();
+	drawSprites();
 
 
-  
- 
-   
+}
+
+function keyPressed() {
+	//write code here
+	if (keyCode === LEFT_ARROW) {
+		fairy.x = fairy.x - 10;
+	}
+
+	if (keyCode === RIGHT_ARROW) {
+		fairy.x = fairy.x + 10;
+	}
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody, false);
+		// star.velocityY=8
+	}
+
+
 }
